1dc8b366cde3e2506106381f8b3e0623b5c2167a
shangkuku <007_gudan@163.com>
2017-05-16 23:16:01 +0800

    Initial commit

---
+++ src/main/java/chatroom/LogUtils.java
@@ -0,0 +1,11 @@
+package chatroom;
+
+/**
+ * Created by RichardYuan on 2017/5/16 0016.
+ */
+public class LogUtils {
+    public static void log(String msg, Throwable e) {
+        e.printStackTrace();
+        System.out.println(msg);
+    }
+}
